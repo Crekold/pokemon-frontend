eda0fbddebc6d66531f8f3ffe36dbfb992b5310c
Crekold <crekoldalt@gmail.com>
2023-10-26 13:23:31 -0400

    z

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,64 +1,45 @@
 <template>
-  <div class="nav-container mb-3 bg-gray-800 text-white">
-    <nav class="container mx-auto py-2">
-      <!-- Logo -->
-      <div class="flex items-center justify-between">
-        <a class="flex items-center" href="#">
-          <img src="../assets/png-transparent-8-bit-pokemon-pixel-art-poke-ball-others-rectangle-bitcoin-pokemon-removebg-preview (1).png" alt="Mi Logo" class="app-logo mr-2">
-          <span class="text-lg font-semibold">Pokemon Team</span>
-        </a>
+  <div class="bg-gray-900 text-gray-300 w-64 min-h-screen p-4">
+    <!-- Header -->
+    <h1 class="text-2xl font-semibold mb-4 text-white">Admin One</h1>
 
-        <!-- Mobile Toggle Button -->
-        <button @click="showMenu = !showMenu" class="lg:hidden p-1 rounded bg-gray-700 hover:bg-gray-600">
-          &#x2630; <!-- Este es un carácter para el menú "hamburger" -->
-        </button>
-      </div>
+    <!-- Menu Items -->
+    <ul>
+      <li class="mb-2 hover:bg-gray-700 px-2 py-1 rounded"><router-link to="/" class="block text-gray-300 hover:text-white">Home</router-link></li>
+      <li v-if="isAuthenticated" class="mb-2 hover:bg-gray-700 px-2 py-1 rounded"><router-link to="/menu" class="block text-gray-300 hover:text-white">Menú Principal</router-link></li>
+    </ul>
 
-      <!-- Navigation Links -->
-      <div :class="{'hidden': !showMenu}" class="mt-4 lg:flex lg:mt-0">
-        <ul class="lg:flex lg:gap-6">
-          <li>
-            <router-link to="/" class="text-white hover:text-gray-400">Home</router-link>
-          </li>
-          <li v-if="isAuthenticated">
-            <router-link to="/menu" class="text-white hover:text-gray-400">Menú Principal</router-link>
-          </li>
-        </ul>
+    <!-- Auth0 Login -->
+    <ul class="lg:flex lg:gap-6 lg:ml-auto mt-4 lg:mt-0">
+      <li v-if="!isAuthenticated && !isLoading">
+        <button @click.prevent="login" class="bg-green-500 hover:bg-green-600 px-4 py-2 rounded text-white">Login</button>
+      </li>
 
-        <!-- Right-aligned links -->
-        <ul class="lg:flex lg:gap-6 lg:ml-auto mt-4 lg:mt-0">
-          <li v-if="!isAuthenticated && !isLoading">
-            <button @click.prevent="login" class="bg-green-500 hover:bg-green-600 px-4 py-2 rounded text-white">Login</button>
-          </li>
+      <li v-if="isAuthenticated" class="relative group">
+    <a href="#" class="flex items-center gap-2 cursor-pointer">
+        <img :src="user.picture" alt="User's profile picture" class="w-10 h-10 rounded-full">
+    </a>
+    <div class="absolute right-0 mt-2 w-48 bg-white text-black p-2 rounded shadow-lg group-hover:flex flex-col hidden z-50">
 
-          <li v-if="isAuthenticated" class="relative group">
-            <a href="#" class="flex items-center gap-2 cursor-pointer">
-              <img :src="user.picture" alt="User's profile picture" class="w-10 h-10 rounded-full">
-            </a>
-            <div class="absolute right-0 mt-2 w-48 bg-white text-black p-2 rounded shadow-lg group-hover:block hidden">
-              <p class="text-black text-sm mb-2">{{ user.name }}</p>
-              <router-link to="/profile" class="block text-black hover:bg-gray-200 p-2 rounded">Profile</router-link>
-              <a href="#" @click.prevent="logout" class="block text-black hover:bg-gray-200 p-2 rounded">Log out</a>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
+        <p class="text-black text-sm mb-2">{{ user.name }}</p>
+        <router-link to="/profile" class="block text-black hover:bg-gray-200 p-2 rounded">Profile</router-link>
+        <a href="#" @click.prevent="logout" class="block text-black hover:bg-gray-200 p-2 rounded">Log out</a>
+    </div>
+</li>
+
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { ref } from 'vue';
 import { useAuth0 } from '@auth0/auth0-vue';
 
 export default {
   name: "NavBar",
   setup() {
-    const showMenu = ref(false);
     const auth0 = useAuth0();
-
+    
     return {
-      showMenu,
       isAuthenticated: auth0.isAuthenticated,
       isLoading: auth0.isLoading,
       user: auth0.user,
@@ -72,14 +53,11 @@ export default {
           }
         });
       }
-    };
+    }
   }
 };
 </script>
 
 <style>
-.app-logo {
-  width: 50px;
-  height: 50px;
-}
+/* Puedes agregar estilos adicionales aquí si lo necesitas */
 </style>
