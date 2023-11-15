36c0124871856bf6e101cddf0e8313a6ffa2478d
Crekold <crekoldalt@gmail.com>
2023-11-15 00:48:09 -0400

    cambios

--- src/views/AgregarTeam.vue
+++ src/views/AgregarTeam.vue
@@ -1,29 +1,47 @@
-<template>
-    <div class="container my-4">
-      <div class="row">
-        <h2 class="col-12">Tu selección:</h2>
-        <div class="col-12 mb-3">
-          <!-- Muestra los Pokémon seleccionados -->
-          <div class="pokemon-card" v-for="pokemon in team" :key="pokemon.id">
-            <img :src="pokemon.image" :alt="pokemon.name" class="pokemon-image" />
-            <div>{{ pokemon.name }}</div>
+ <template>
+     <div class="container my-4">
+    <div class="row mb-3">
+      <h2 class="col-12">Tu selección:</h2>
+      <!-- Aquí se mostrarán los Pokémon seleccionados -->
+      <div class="col-md-1-5" v-for="pokemon in team" :key="pokemon.id">
+        <div class="card">
+          <img :src="pokemon.image" :alt="pokemon.name" class="card-img-top" />
+          <div class="card-body">
+            <h5 class="card-title">{{ pokemon.name }}</h5>
+            <p class="card-text">
+              <!-- Muestra los tipos del Pokémon seleccionado -->
+              <span v-for="type in pokemon.type" :key="type" class="badge bg-secondary">{{ type }}</span>
+            </p>
             <button class="btn btn-danger" @click="removeFromTeam(pokemon)">Quitar</button>
           </div>
         </div>
-        <h2 class="col-12">Disponibles:</h2>
-        <div class="col-12">
-          <!-- Muestra los Pokémon disponibles -->
-          <div class="pokemon-card" v-for="pokemon in availablePokemons" :key="pokemon.id">
-            <img :src="pokemon.image" :alt="pokemon.name" class="pokemon-image" />
-            <div>{{ pokemon.name }}</div>
+      </div>
+      
+    </div>
+    <div class="row">
+      <h2 class="col-12">Disponibles:</h2>
+      <div class="col-md-1-5" v-for="pokemon in availablePokemons" :key="pokemon.id">
+        <div class="card">
+          <img :src="pokemon.image" :alt="pokemon.name" class="card-img-top" />
+          <div class="card-body">
+            <h5 class="card-title">{{ pokemon.name }}</h5>
+            <p class="card-text">
+              <span v-for="type in pokemon.type" :key="type" class="badge badge-primary mr-1">
+                {{ type }}
+              </span>
+            </p>
+
             <button class="btn btn-primary" @click="addToTeam(pokemon)">Agregar</button>
           </div>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
+
+
   
-  <script lang="ts">
+<script lang="ts">
   import { defineComponent, ref, onMounted } from 'vue';
   import axios from 'axios';
   
@@ -88,12 +106,22 @@
   </script>
   
   <style scoped>
-  .pokemon-card {
-    /* Tus estilos para las tarjetas de pokémon */
-  }
-  
-  .pokemon-image {
-    /* Estilos para la imagen del pokémon */
-  }
+ .col-md-1-5 {
+  flex: 0 0 auto;
+  width: 12.5%;
+}
+
+.card {
+  margin: 0.5rem;
+}
+
+/* Ajusta los estilos de la imagen para que encaje bien en la tarjeta */
+.card-img-top {
+  width: 100%;
+  height: auto;
+}
   </style>
+  
+
+
   
