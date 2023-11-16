8f5db3fa0126b4514ea0ecb7999b92efc2c480f0
Crekold <crekoldalt@gmail.com>
2023-11-16 09:29:10 -0400

    modificaciones

--- src/views/AgregarTeam.vue
+++ src/views/AgregarTeam.vue
@@ -1,23 +1,31 @@
 <template>
     <div class="container my-4">
+    <!-- Campo para ingresar el nombre del equipo -->
+    <div class="row mb-3">
+      <div class="col-12">
+        <input type="text" class="form-control mb-3" placeholder="Nombre del equipo" v-model="teamName" />
+      </div>
+    </div>
       <!-- Sección de búsqueda -->
       <div class="row mb-3">
         <div class="col-12">
           <input type="text" class="form-control" placeholder="Buscar Pokémon..." v-model="searchQuery" />
         </div>
       </div>
-  
+      
       <!-- Sección de Pokémon seleccionados -->
       <div class="row mb-3">
         <div class="col-12">
       <div class="card">
         <div class="card-header">
           <h2>Tu selección:</h2>
+          <button class="btn btn-success" @click="createTeam" :disabled="team.length === 0">Crear equipo</button>
         </div>
         <div class="card-body">
           <div class="row">
             <div class="col-md-1-5" v-for="pokemon in team" :key="pokemon.id">
               <div class="card">
+
                 <img :src="pokemon.image" class="card-img-top" :alt="pokemon.name" />
                 <div class="card-body">
                   <h5 class="card-title">{{ pokemon.name }}</h5>
@@ -40,8 +48,15 @@
         <div class="col-12">
           <h2>Disponibles:</h2>
         </div>
-        <div class="col-md-1-5" v-for="pokemon in filteredPokemons" :key="pokemon.id">
-      <div class="card">
+<!-- Indicador de carga para Pokémon disponibles -->
+<div v-if="isLoading" class="col-12 text-center">
+        <div class="spinner-border text-primary" role="status">
+          <span class="visually-hidden"></span>
+        </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4 col-lg-3" v-for="pokemon in filteredPokemons" :key="pokemon.id">
+      <div class="card h-100">
         <img :src="pokemon.image" class="card-img-top" :alt="pokemon.name" />
         <div class="card-body">
           <h5 class="card-title">{{ pokemon.name }}</h5>
@@ -75,10 +90,12 @@
   export default defineComponent({
     name: 'PokemonTeamSelector',
     setup() {
+      const isLoading = ref(true);
       const team = ref<Pokemon[]>([]);
       const availablePokemons = ref<Pokemon[]>([]);
       const baseURL = 'https://pokeapi.co/api/v2/pokemon/';
       const searchQuery = ref('');
+      const teamName = ref('');
 
       const filteredPokemons = computed(() => {
       if (!searchQuery.value) {
@@ -105,8 +122,11 @@
         } catch (error) {
           console.error(error);
         }
+        finally {
+          isLoading.value = false;
+        }
       };
-  
+
       const addToTeam = (pokemon: Pokemon) => {
         if (team.value.length < 6) {
           team.value.push(pokemon);
@@ -115,7 +135,51 @@
           alert('Tu equipo ya tiene 6 pokémons.');
         }
       };
-  
+      const createTeam = async () => {
+  if (team.value.length === 0) {
+    alert('No puedes crear un equipo vacío.');
+    return;
+  }
+  if (teamName.value.trim() === '') {
+    alert('Por favor ingresa un nombre para el equipo.');
+    return;
+  }
+
+  try {
+    // Suponemos que la API espera un campo "team_name" para el nombre del equipo
+    const teamResponse = await axios.post('/api/teams', {
+      team_name: teamName.value,
+      // Aquí puedes agregar otros campos necesarios como el 'user_id', si es necesario
+    });
+
+    // Verificar que el backend devuelve el 'team_id'
+    if (teamResponse.data && teamResponse.data.team_id) {
+      const teamId = teamResponse.data.team_id;
+
+      // Asociar cada Pokémon seleccionado con el equipo creado
+      const associationPromises = team.value.map(pokemon => {
+        return axios.post('/api/team_pokemon', {
+          team_team_id: teamId,
+          pokemon_pokemon_id: pokemon.id,
+        });
+      });
+
+      // Esperar a que todas las asociaciones estén completas
+      await Promise.all(associationPromises);
+
+      alert('Equipo creado con éxito.');
+      // Aquí podrías limpiar el formulario o redirigir al usuario
+    } else {
+      // Si no se obtiene un 'team_id', manejar el error adecuadamente
+      alert('Error al crear el equipo. No se obtuvo el ID del equipo.');
+    }
+  } catch (error) {
+    console.error('Hubo un error al crear el equipo:', error);
+    // Manejar otros errores, como problemas de red o de servidor
+    alert('Error al crear el equipo. Por favor, inténtalo de nuevo.');
+  }
+};
+
      const removeFromTeam = (pokemon: Pokemon) => {
   // Añade el Pokémon de nuevo a la lista de disponibles
   availablePokemons.value.push(pokemon);
@@ -134,9 +198,12 @@
         team,
         availablePokemons,
         addToTeam,
+        isLoading,
         searchQuery,
         filteredPokemons,   
         removeFromTeam,
+        teamName,
+        createTeam,
       };
       
     },
@@ -144,25 +211,40 @@
     typeColor(type: string) {
       return `type-${type}`;
     },
+    
   },
   });
   </script>
   
   <style scoped>
- .col-md-1-5 {
-  flex: 0 0 auto;
-  width: 12.5%;
-}
-
 .card {
-  margin: 0.5rem;
+  display: flex;
+  flex-direction: column;
 }
 
-/* Ajusta los estilos de la imagen para que encaje bien en la tarjeta */
+.card-body {
+  flex-grow: 1;
+}
+
+/* Asegúrate de que las imágenes de los Pokémon se ajusten bien dentro de las tarjetas */
 .card-img-top {
-  width: 100%;
+  width: 100%; /* o podría ser auto dependiendo de tus necesidades */
   height: auto;
 }
+
+@media (max-width: 768px) {
+  .col-md-1-5 {
+    width: 50%; /* O ajusta según tu diseño específico */
+  }
+}
+
+@media (max-width: 576px) {
+  .col-md-1-5 {
+    width: 100%; /* O ajusta según tu diseño específico */
+  }
+}
+
+/* Estilos adicionales para el botón si son necesarios */
 
 .type-normal { background-color: #A8A77A; }
 .type-fire { background-color: #EE8130; }
