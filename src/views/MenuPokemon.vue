ea87e5324440a6598bd4bebcc11dff13e4a30eaa
Crekold <crekoldalt@gmail.com>
2023-11-30 23:08:08 -0400

    Add Pokemon images and background to MenuPokemon.vue

--- src/views/MenuPokemon.vue
+++ src/views/MenuPokemon.vue
@@ -1,22 +1,42 @@
 <template>
-  <div>
+  <div class="background">
     <div class="container my-5">
       <h1 class="text-center mb-4">Menu Principal</h1>
 
       <div class="d-flex flex-column align-items-center">
-        <button class="btn btn-primary btn-lg mb-2" @click="addTeam">
-          <font-awesome-icon :icon="['fas', 'plus']" class="mr-2" /> Agregar Equipo
-        </button>
-        <button class="btn btn-secondary btn-lg mb-2" @click="viewTeams">
-          Tus equipos
-        </button>
-        <button class="btn btn-danger btn-lg mt-3" @click="logout">
-          <font-awesome-icon :icon="['fas', 'sign-out-alt']" class="mr-2" /> Logout
-        </button>
+        <div class="card" style="width: 18rem;">
+          <div class="d-flex justify-content-center align-items-center" style="height: 100px;">
+          <img src="https://i.pinimg.com/originals/02/c7/05/02c70572482476ba3ec8c4962f883705.png" class="card-img-top pokemon-image" alt="pokemon-1">
+          </div>
+          <div class="card-body">
+            <button class="btn btn-primary btn-lg mb-2" @click="addTeam">
+              <font-awesome-icon :icon="['fas', 'plus']" class="mr-2" /> Agregar Equipo
+            </button>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <div class="d-flex justify-content-center align-items-center" style="height: 100px;">
+          <img src="https://images.wikidexcdn.net/mwuploads/wikidex/thumb/c/c9/latest/20130204042256/EP672_Guarder%C3%ADa_Pok%C3%A9mon.png/250px-EP672_Guarder%C3%ADa_Pok%C3%A9mon.png" class="card-img-top pokemon-image" alt="pokemon-2">
+        </div>
+          <div class="card-body">
+            <button class="btn btn-secondary btn-lg mb-2" @click="viewTeams">
+              Tus equipos
+            </button>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img src="ruta-de-la-imagen-del-pokemon-3" class="card-img-top pokemon-image" alt="pokemon-3">
+          <div class="card-body">
+            <button class="btn btn-danger btn-lg mt-3" @click="logout">
+              <font-awesome-icon :icon="['fas', 'sign-out-alt']" class="mr-2" /> Logout
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
 
 <script lang="ts">
 import { defineComponent } from 'vue';
@@ -58,4 +78,17 @@ export default defineComponent({
 
 <style scoped>
 /* Estilos del componente (si los tienes) */
+.pokemon-image {
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+}
+
+.background {
+  background-image: url('https://i.pinimg.com/originals/ab/58/cc/ab58cc75dcb2e42555cf7e614216350a.jpg');
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+}
+
 </style>
